7916eeb9e2c3febfbfb65310afff262354a1ff2e
Lauren Yeung <laurenyeung217@gmail.com>
2024-08-08 00:08:09 +0000

    Add recipe form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,12 @@
 // Initialize the side navigation bar
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-});
-
-/**
- * This is the function to display the flash message
- */
-$(document).ready(function() {
-    $('#flash-messages .flash-message').each(function() {
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+    // This is the function to display the flash message
+    $('#flash-messages .flash-message').each(function () {
         alert($(this).text());
     });
 });
+
+

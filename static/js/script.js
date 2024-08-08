632eac9cc961f0270b3a2a7069a2f97725f5389e
Lauren Yeung <laurenyeung217@gmail.com>
2024-08-08 20:10:57 +0000

    Add custom Materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
      * When the button click 
      * it create a new input element for ingredients
      */
-    $('#add-ingredient').on('click', function() {
+    $('#add-ingredient').on('click', function () {
         ingredientCount++;
         let ingredientInput = $('<input>', {
             type: 'text',
@@ -29,12 +29,12 @@ $(document).ready(function () {
         });
         $('#ingredients-section').append(ingredientInput)
     });
-    
+
     /**
      * Event handler for the "Add Step" button
      * it create a new textarea element for preparation steps
      */
-    $('#add-step').on('click', function() {
+    $('#add-step').on('click', function () {
         stepCount++;
         let stepTextarea = $('<textarea>', {
             name: 'preparation_steps',
@@ -46,6 +46,47 @@ $(document).ready(function () {
         $('#preparation-section').append(stepTextarea);
     });
 
+    // This code was copied from Code Institude on 08-08-2024.
+    // Original Author: Tim Nelson
+    // Source: https://github.com/Code-Institute-Solutions/TaskManagerAuth/blob/main/04-AddingATask-WritingToTheDatabase/02-materialize-select-validation/static/js/script.js
+    validateMaterializeSelect();
+
+    function validateMaterializeSelect() {
+        let classValid = {
+            "border-bottom": "1px solid #4caf50",
+            "box-shadow": "0 1px 0 0 #4caf50"
+        };
+        let classInvalid = {
+            "border-bottom": "1px solid #f44336",
+            "box-shadow": "0 1px 0 0 #f44336"
+        };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({
+                "display": "block",
+                "height": "0",
+                "padding": "0",
+                "width": "0",
+                "position": "absolute"
+            });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
-
-

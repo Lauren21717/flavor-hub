b010e323800c069a67df6e50820ddd1402fcdc7a
Lauren Yeung <laurenyeung217@gmail.com>
2024-08-08 01:00:40 +0000

    Implement add ingredient and add step button functionalities, add functionality to dynamically add new input and textarea.

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,51 @@
-// Initialize the side navigation bar
 $(document).ready(function () {
+    // Initialize the side navigation bar
     $('.sidenav').sidenav({
         edge: "right"
     });
+    $('select').formSelect();
+
     // This is the function to display the flash message
     $('#flash-messages .flash-message').each(function () {
         alert($(this).text());
     });
-    $('select').formSelect();
+
+    let ingredientCount = 1;
+    let stepCount = 1;
+    /**
+     * Event handler for the "Add Ingredient button"
+     * When the button click 
+     * it create a new input element for ingredients
+     */
+    $('#add-ingredient').on('click', function() {
+        ingredientCount++;
+        let ingredientInput = $('<input>', {
+            type: 'text',
+            name: 'ingredients',
+            class: 'validate',
+            minlength: '5',
+            maxlength: '50',
+            required: true
+        });
+        $('#ingredients-section').append(ingredientInput)
+    });
+    
+    /**
+     * Event handler for the "Add Step" button
+     * it create a new textarea element for preparation steps
+     */
+    $('#add-step').on('click', function() {
+        stepCount++;
+        let stepTextarea = $('<textarea>', {
+            name: 'preparation_steps',
+            class: 'validate materialize-textarea',
+            minlength: '5',
+            maxlength: '200',
+            required: true
+        });
+        $('#preparation-section').append(stepTextarea);
+    });
+
 });
 
 
